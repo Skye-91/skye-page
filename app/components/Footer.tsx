0b2b47465e417ed1195ceb789e864779431a842a
Skye-91 <jeshoua2001@gmail.com>
2024-10-15 11:36:37 +0200

    Created the base site and installed DaisyUI

---
+++ app/components/Footer.tsx
@@ -0,0 +1,45 @@
+export default function Footer() {
+	return (
+		<footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
+			<nav className="grid grid-flow-col gap-4">
+				<a
+					href="https://x.com/Sky_9911"
+					className="link link-hover underline"
+				>
+					Twitter
+				</a>
+				<a
+					href="https://skye-page.neocities.org/"
+					className="link link-hover underline"
+				>
+					Neocities Page
+				</a>
+			</nav>
+			<aside>
+				<p>
+					Created with{" "}
+					<a
+						className="link link-hover text-primary font-bold"
+						href="https://react.dev/"
+					>
+						React
+					</a>
+					,{" "}
+					<a
+						className="link link-hover text-accent font-bold"
+						href="https://nextjs.org/"
+					>
+						NextJS
+					</a>{" "}
+					and{" "}
+					<a
+						className="link link-hover text-secondary font-bold"
+						href="https://daisyui.com"
+					>
+						DaisyUI
+					</a>
+				</p>
+			</aside>
+		</footer>
+	)
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import daisyui from "daisyui"
+
+export const daisyuiThemes = [
+	"synthwave",
+	"retro",
+	"valentine",
+	"cyberpunk",
+	"aqua",
+	"dim",
+	"coffee",
+]
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [daisyui],
+	daisyui: {
+		themes: daisyuiThemes,
+	},
+}
+export default config
