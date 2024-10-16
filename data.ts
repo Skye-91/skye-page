9d2ea572a10a9ee651e4381dcf5edb22b587d0b5
Skye-91 <101327498+Skye-91@users.noreply.github.com>
2024-10-16 20:48:51 +0200

    Updated data

--- data.ts
+++ data.ts
@@ -19,6 +19,14 @@ export const games: Game[] = [
 		review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br><br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br><br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br><br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	},
 	{
+		title: "AI: The Somnium Files",
+		score: 9,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl: "https://howlongtobeat.com/games/65827_AI_The_Somnium_Files.jpg?width=250",
+	},
+	{
 		title: "Borderlands The Pre-Sequel",
 
 		status: "Dropped",
