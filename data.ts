e21708d6f9ae7468de3250da659d0347d0b05f39
Skye-91 <101327498+Skye-91@users.noreply.github.com>
2024-10-16 15:30:37 +0200

    Updated data

--- data.ts
+++ data.ts
@@ -1,4 +1,4 @@
-import { Game } from "./app/components/Card"
+import { Game } from "./app/components/Card";
 
 export const games: Game[] = [
 	{
@@ -11,4 +11,307 @@ export const games: Game[] = [
 			"https://howlongtobeat.com/games/Persona5visual.jpg?width=250",
 		review: "",
 	},
-]
+	{
+		title: "Severed Steel",
+		score: 8,
+		status: "Completed",
+		tags: ["Singleplayer"],
+		timesCompleted: 1.8,
+		imageUrl:
+			"https://howlongtobeat.com/games/89786_Severed_Steel.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "X4 Foundations",
+		score: 8,
+		tags: ["Singleplayer"],
+		timesCompleted: 3,
+		imageUrl:
+			"https://howlongtobeat.com/games/62872_X4_Foundations.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Borderlands The Pre-Sequel",
+		score: -1,
+		status: "In Progress",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Borderlands_The_Pre-Sequel_box_art.jpg?width=250",
+		review: "",
+	},
+
+	{
+		title: "Battlefield: Bad Company 2",
+		score: 6,
+		status: "Dropped",
+		tags: ["Singleplayer", "Multiplayer"],
+		imageUrl: "https://howlongtobeat.com/games/Bc2lowres.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "BioShock",
+		score: 7.5,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Bioshockcoverfinalcropped.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Black Mesa",
+		score: 8,
+		status: "Completed",
+		timesCompleted: 1.75,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/1100_Black_Mesa.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Boneworks",
+		score: 8,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer", "VR"],
+		imageUrl:
+			"https://howlongtobeat.com/games/69535_BONEWORKS.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Borderlands 2",
+		score: 2,
+		status: "Dropped",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Borderlands2boxart3.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Cities: Skylines",
+		score: 7,
+		tags: ["Singleplayer", "Endless"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Cities_Skylines_cover_art.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Cyberpunk 2077",
+		score: 7.5,
+		status: "In Progress",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Cyberpunk-2077-2.jpg?width=250",
+		review: "Tecnically the first playthrough is not finished. The story is meh, but the gameplay is fun.",
+	},
+	{
+		title: "Deponia (+ Doomsday)",
+		score: 7.8,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl: "https://howlongtobeat.com/games/2448_Deponia.jpg?width=250",
+		review: "It's a shame that the company closed down because of Gollum.",
+	},
+	{
+		title: "Destiny 2",
+		score: 6,
+		status: "Completed",
+		tags: ["Singleplayer", "Multiplayer", "Live Service"],
+		imageUrl:
+			"https://howlongtobeat.com/games/43894_Destiny_2.png?width=250",
+		review: "",
+	},
+	{
+		title: "Devil May Cry 5",
+		score: -1,
+		status: "In Progress",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/57514_Devil_May_Cry_5.jpg?width=250",
+		review: "The controls and combos are difficoult to remember.",
+	},
+	{
+		title: "Doki Doki Literature Club",
+		score: 8.5,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/49377_Doki_Doki_Literature_Club.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Doom (2016)",
+		score: 8,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl: "https://howlongtobeat.com/games/doom_2016.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Doom Eternal",
+		score: 8.5,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/57506_Doom_Eternal.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "The Elder Scrolls V: Skyrim",
+		score: 8,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/TheElderScrollsVSkyrimLegendaryEdition.jpg?width=250",
+		review: "A lot of mods.",
+	},
+	{
+		title: "Elite Dangerous",
+		score: 9,
+		tags: ["Singleplayer", "Multiplayer", "Endless"],
+		imageUrl:
+			"https://howlongtobeat.com/games/EliteDangerous.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Euro Truck Simulator 2",
+		score: 7.5,
+		tags: ["Singleplayer", "Endless"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Euro_Truck_Simulator_2_cover.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Fallout 4",
+		score: 7.7,
+		status: "Completed",
+		timesCompleted: 2,
+		tags: ["Singleplayer"],
+		imageUrl: "https://howlongtobeat.com/games/Fallout_4.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Fallout: New Vegas",
+		score: -1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/3351_Fallout_New_Vegas.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "The First Descendant",
+		score: 5,
+		tags: ["Live Service"],
+		imageUrl:
+			"https://howlongtobeat.com/games/115058_The_First_Descendant.jpg?width=250",
+		review: "Too grindy and poor performance.",
+	},
+	{
+		title: "Game Dev Tycoon",
+		score: 6.9,
+		tags: ["Singleplayer", "Endless"],
+		imageUrl:
+			"https://howlongtobeat.com/games/2481853-box_gdt.png?width=250",
+		review: "",
+	},
+	{
+		title: "Garry's Mod",
+		score: 8.5,
+		tags: ["Singleplayer", "Endless"],
+		imageUrl:
+			"https://howlongtobeat.com/games/GarrysMod_292x136.jpg?width=250",
+		review: "A lot of mods.",
+	},
+	{
+		title: "God Eater 3",
+		score: 4,
+		status: "Dropped",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/64937_God_Eater_3.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Grand Theft Auto IV",
+		score: -1,
+		status: "In Progress",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/4062_Grand_Theft_Auto_IV.png?width=250",
+		review: "",
+	},
+	{
+		title: "Grand Theft Auto V",
+		score: 8,
+		status: "Completed",
+		timesCompleted: 2,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/4064_Grand_Theft_Auto_V.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Grand Theft Auto: San Andreas",
+		score: -1,
+		status: "In Progress",
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/4073_Grand_Theft_Auto_San_Andreas.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Hacknet",
+		score: 7.9,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/Hacknet_header.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Half-Life (+ Blue Shift & Opposing Force)",
+		score: 7.8,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/256px-Half-Life_Cover_Art.jpg?width=250",
+		review: "Revolutionary game for it's time, but the last part is not good.",
+	},
+	{
+		title: "Half-Life 2 (+ Episode 1 & 2 & Lost Coast)",
+		score: 8.2,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/256px-421px-HL2box.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "Half-Life Alyx",
+		score: 8.5,
+		status: "Completed",
+		timesCompleted: 1.5,
+		tags: ["Singleplayer", "VR"],
+		imageUrl:
+			"https://howlongtobeat.com/games/72067_Half-Life_Alyx.jpg?width=250",
+		review: "",
+	},
+	{
+		title: "A Hat in Time",
+		score: 8.3,
+		status: "Completed",
+		timesCompleted: 1,
+		tags: ["Singleplayer"],
+		imageUrl:
+			"https://howlongtobeat.com/games/A_Hat_in_Time_logo.png?width=250",
+		review: "Cute.",
+	},
+];
